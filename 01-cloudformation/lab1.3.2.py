ed886deafc6b0be92ea48ab5f386c89ae768e721
Gary Ringer <gary.ringer@us.idemia.com>
2021-10-28 21:30:15 -0500

    Lab 1.3.3

--- 01-cloudformation/lab1.3.2.py
+++ 01-cloudformation/lab1.3.2.py
@@ -8,12 +8,12 @@ if __name__ == "__main__": # https://www.freecodecamp.org/news/if-name-main-pyth
         print(region)
         aws = boto3.client('cloudformation', region) # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudformation.html
         try:
-            aws.create_stack(StackName='gr-m01x32-training',TemplateBody=open('cloudformation1.3.1.yaml').read() # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudformation.html#CloudFormation.Client.create_stack
-        )
+            aws.create_stack(StackName='gr-m01x32-training',TemplateBody=open('cloudformation1.3.1.yaml').read()) # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudformation.html#CloudFormation.Client.create_stack
+
         except aws.exceptions.AlreadyExistsException: # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/error-handling.html
 
             try:
-                aws.update_stack(StackName='gr-m01x32-training', TemplateBody=open('cloudformation1.3.1.yml').read() # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudformation.html#CloudFormation.Client.update_stack
-                )
+                aws.update_stack(StackName='gr-m01x32-training', TemplateBody=open('cloudformation1.3.1.yml').read()) # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudformation.html#CloudFormation.Client.update_stack
+
             except aws.exceptions.ClientError as e: # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/error-handling.html
                 print(e)
